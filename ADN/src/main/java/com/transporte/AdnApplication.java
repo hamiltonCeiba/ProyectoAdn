39edc9bed4c4d2cb0c27244491a6d6038bb7ca49
hjojoa <hjojoa@unicauca.edu.co>
2019-09-01 20:38:35 -0500

    proyecto con guía

---
+++ ADN/src/main/java/com/transporte/AdnApplication.java
@@ -0,0 +1,13 @@
+package com.transporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+public class AdnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdnApplication.class, args);
+	}
+
+}
